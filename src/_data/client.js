be8c5209deabd1f7bc7507b836fa83314534f96a
Doc1998 <docmp98@gmail.com>
2026-06-04 15:15:20 +0100

    updates

---
+++ src/_data/client.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "name": "Path to Peace | Online Therapy Services Ireland",
+    "email": "",
+    "phoneForTel": "0874885445",
+    "phoneFormatted": "(087) 488 5445",
+    "address": {
+        "lineOne": "",
+        "lineTwo": "",
+        "city": "Kerry",
+        "state": "",
+        "zip": "",
+        "country": "IE",
+        "mapLink": ""
+    },
+    "socials": {
+        "facebook": "",
+        "instagram": "https://www.instagram.com/",
+    },
+    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+    "domain": ""
+};
